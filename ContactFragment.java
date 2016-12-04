2dafdedf8fbf25bd14ee2d45aef5762f474a7733
AlbionaHoti <albionahh@gmail.com>
2016-12-04 22:25:02 +0100

    File rreth krijimit te contact activity dhe home activity.
    File e contact, ku eshte bere dizajnimi i contact dhe lidhja e main activity me contact. Po ashtu dhe krijimi i home activity ku eshte bere lidhja prej main activity me home.

---
+++ ContactFragment.java
@@ -0,0 +1,24 @@
+package com.example.ermira.hotigroup;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ContactFragment extends Fragment {
+
+    public ContactFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_contact, container, false);
+
+        return rootView;
+    }
+
+}
